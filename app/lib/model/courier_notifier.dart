930d8cc81047d4681c235eb6a24971ec5be3f304
tigranhov <thov88@gmail.com>
2023-06-09 11:50:57 +0400

    dasf

--- app/lib/model/courier_notifier.dart
+++ app/lib/model/courier_notifier.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import 'api.dart';
@@ -7,9 +8,15 @@ part 'courier_notifier.g.dart';
 
 @riverpod
 class CourierNotifier extends _$CourierNotifier {
+  AudioPlayer? player;
   @override
   Future<Courier?> build() async {
     final api = ref.watch(apiProvider);
+    player = AudioPlayer();
+    ref.onDispose(() {
+      player?.dispose();
+    });
+
     //TODO timer for demonstration, remove later
     await Future.delayed(const Duration(seconds: 1));
     final response = await api.get('/auth/me');
@@ -19,6 +26,9 @@ class CourierNotifier extends _$CourierNotifier {
   }
 
   void changeStatus([bool? active]) async {
+    if (player?.source == null) {
+      await player?.setSource(AssetSource('sounds/go_online.mp3'));
+    }
     state = await AsyncValue.guard(() async {
       final courier = state.value!;
       final dio = ref.read(apiProvider);
@@ -26,6 +36,10 @@ class CourierNotifier extends _$CourierNotifier {
       final targetStatus = activeState ? 'ACTIVE' : 'INACTIVE';
       final response = await dio
           .patch('/couriers/${courier.id}', data: {'status': targetStatus});
+      if (targetStatus == 'ACTIVE') {
+        await player?.stop();
+        player?.resume();
+      }
       return Courier.fromJson(response.data['data']);
     });
   }
